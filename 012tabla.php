b56cbb22e59cb5b0e64b431d2263fd4102919245
Ignacio Fernandez <fernandezignacio2000@gmail.com>
2023-09-28 10:41:19 +0200

    012

---
+++ 012tabla.php
@@ -0,0 +1,14 @@
+/*012formulario.html y 012tabla.php: A partir de un número de filas y
+columnas, crear una tabla con ese tamaño. Las celdas deben estar
+rellenadas con los valores de las coordenadas de cada celda.*/
+
+
+
+
+
+
+
+
+
+
+
