d8aba8d8ef34f7f5cda858133fb07a644cd5a4b0
Ignacio Fernandez <fernandezignacio2000@gmail.com>
2023-09-28 10:55:26 +0200

    001

--- Cliente/001requieredElse.js
+++ Cliente/001requieredElse.js
@@ -10,10 +10,15 @@ function checkAge(age) {
   }
 }
 ¿Funcionará la función de manera diferente si se borra else? 
-¿Hay alguna diferencia en el comportamiento de estas dos variantes?*/
+¿Hay alguna diferencia en el comportamiento de estas dos variantes?
 
 
 
+En este caso vemos claramente como la función va a devolver en casi de 
+que la edad sea mayor que 18, va a retornar true, en caso de que no lo sea 
+va a soliciar confirmación parental. En el caso de que quitásemos el "else",
+funcionaría exactamente igual pero el código no sería tan claro y podría
+llegar a confundir a un programador que no esté en un nivel avanzado todavía.
 
 
 
