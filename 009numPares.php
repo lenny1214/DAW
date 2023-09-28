06f01837ec798038d6f5a8823e8d6c5f541f6e2e
Ignacio Fernandez <fernandezignacio2000@gmail.com>
2023-09-28 09:37:22 +0200

    009

---
+++ 009numPares.php
@@ -0,0 +1,13 @@
+/*009numPares.php: Escribe un programa que muestre los números pares del
+0 al 50 (dentro de una lista desordenada).*/
+
+
+
+<?php
+for($ind=0;$ind<=50;$ind+=2){
+    echo "$ind";
+}
+
+
+
+?>
