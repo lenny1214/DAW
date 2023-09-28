b7d8fbfbf1f91521c4f03d13b119de3e5bc44201
Ignacio Fernandez <fernandezignacio2000@gmail.com>
2023-09-28 10:55:53 +0200

    001

--- Cliente/001requieredElse.js
+++ Cliente/001requieredElse.js
@@ -18,19 +18,7 @@ En este caso vemos claramente como la función va a devolver en casi de
 que la edad sea mayor que 18, va a retornar true, en caso de que no lo sea 
 va a soliciar confirmación parental. En el caso de que quitásemos el "else",
 funcionaría exactamente igual pero el código no sería tan claro y podría
-llegar a confundir a un programador que no esté en un nivel avanzado todavía.
-
-
-
-
-
-
-
-
-
-
-
-
+llegar a confundir a un programador que no esté en un nivel avanzado todavía.*/
 
 
 
