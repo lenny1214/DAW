1f7699ad0d1c45f93135481ffad62431b420fae5
Ignacio Fernandez <fernandezignacio2000@gmail.com>
2023-09-28 09:17:22 +0200

    008

--- 008investigar.php
+++ 008investigar.php
@@ -3,8 +3,20 @@
 con un par de l√≠neas su propósito y mediante código demuestra su uso.*/
 
 
+<?php
+$opcion = "b";
+
+$resultado = match ($opcion) {
+    "a" => "Opción A seleccionada",
+    "b" => "Opción B seleccionada",
+    "c" => "Opción C seleccionada",
+    default => "Opción no válida",
+};
+
+echo $resultado; // Imprime "Opción B seleccionada"
 
 
+?>
 
 
 
