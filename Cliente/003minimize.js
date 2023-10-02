200d46fda7f25fe62a67a87500b776f2bbc9e30e
Ignacio Fernandez <fernandezignacio2000@gmail.com>
2023-10-02 13:30:31 +0200

    MM

--- Cliente/003minimize.js
+++ Cliente/003minimize.js
@@ -16,6 +16,13 @@ min(1, 1) == 1*/
 
 
 function min(a,b) {
+    if (a < b) {
+        return a;
+      } else {
+        return b;
+      }
+    }
 
-}
-
+    console.log(min(2,5));
+    console.log(min(3, -1));  
+    console.log(min(1, 1));
