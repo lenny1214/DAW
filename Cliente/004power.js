1df0de9484ac1b4dee294483e71c93f5e42e9eb8
Ignacio Fernandez <fernandezignacio2000@gmail.com>
2023-10-02 09:54:08 +0200

    018

---
+++ Cliente/004power.js
@@ -0,0 +1,22 @@
+"use strict"
+
+
+/*004power.js/.html: Función pow(x,n): Escriba la función pow(x,n) que 
+devuelva x como potencia de n. O, 
+en otras palabras, multiplique x por si mismo n veces y 
+devuelva el resultado.
+pow(3, 2) = 3 * 3 = 9
+pow(3, 3) = 3 * 3 * 3 = 27
+pow(1, 100) = 1 * 1 * ...* 1 = 1
+Cree una página web que solicite x y n, y luego muestre el resultado de 
+pow(x,n).
+Nota: La función solo debe admitir valores naturales de n: enteros desde 1.*/
+
+function pow(x,n){
+    
+}
+
+
+
+
+
