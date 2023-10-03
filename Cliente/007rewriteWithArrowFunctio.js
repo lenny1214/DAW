78e79f8356249bcfd068e29e4d69e43fcb1749e2
Ignacio Fernandez <fernandezignacio2000@gmail.com>
2023-10-03 10:44:29 +0200

    007

---
+++ Cliente/007rewriteWithArrowFunctio.js
@@ -0,0 +1,38 @@
+"use strict"
+
+/*007rewriteWithArrowFunction.js/.html:
+ Reemplace las expresiones de función con funciones de flecha 
+ en el código a continuación:
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
+ask(
+  "Do you agree?",
+  function() { alert("You agreed."); },
+  function() { alert("You canceled the execution."); }
+);*/
+
+
+const ask = (question, yes, no) => {
+    if (confirm(question)) yes();
+    else no();
+  };
+  
+  // Ejemplo de uso:
+  ask(
+    "¿Estás de acuerdo?",
+    () => alert("¡Estás de acuerdo!"),
+    () => alert("No estás de acuerdo.")
+  );
+  
+
+
+
+
+
+
+
+
+
