c7a58d03b4a6d531ca8737f6bac0e1352054733f
Ignacio Fernandez <fernandezignacio2000@gmail.com>
2023-10-03 10:55:58 +0200

    008

--- Cliente/008filterEvenNumbers.js
+++ Cliente/008filterEvenNumbers.js
@@ -5,9 +5,15 @@ y devuelva un nuevo array que solo contenga los números pares.
 Utiliza una función flecha para realizar esta operación.*/
 
 
+/*En esta primera línea lo que estamos declarando es el filtro(función predeterminada), que va a ser de arrays y el filtro 
+que le vamos a meter espara que muestre aquellos con los que
+la división entre 2 sea estrictamente igaul*/
+const filterEvenNumbers = (arr) => arr.filtrer((num) => num % 2 === 0);{
+    const numeros=[1,2,3,4,5,6,7,8,9,10];
+    const numPar=filterEvenNumbers(numeros);
+    console.log(numPar);
 
-
-
+}
 
 
 
