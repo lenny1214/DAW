a5cb582090a30de1b333d7beb9692a9cbba42b4a
Ignacio Fernandez <fernandezignacio2000@gmail.com>
2023-10-03 09:35:55 +0200

    018

---
+++ Servidor/018preparaTicket.php
@@ -0,0 +1,49 @@
+/*018preparaTique.php: A partir de una cantidad de productos, leer el
+nombre y coste de la cantidad de productos indicados (la cantidad, se
+recibe mediante un parámetro GET vía URL).
+018imprimeTique.php: Tras leer los datos del tique de compra, enumera en
+una tabla los productos, con su precio en euros y pesetas, y finalmente, en
+una última fila, totalizar en ambas monedas.*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
