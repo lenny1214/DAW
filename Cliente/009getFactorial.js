a37ad163e99b5569719bf5441ccaf7dd394c6ee1
Ignacio Fernandez <fernandezignacio2000@gmail.com>
2023-10-04 10:12:00 +0200

    009

--- Cliente/009getFactorial.js
+++ Cliente/009getFactorial.js
@@ -4,3 +4,23 @@
 
 
  
+function calcularFactorial(num){
+    if(num === 0 || num === 1){ //Comprobamos que si el numero es 0 o 1, nos devuelva su factorial, es decir, 1
+        return 1;
+
+    }else if (num < 1){
+        return "No se puede calcular el factorial de un número negativo"
+    }else {
+
+        let factorial=1;
+        for(let i=1; i<=num; i++){
+            factorial*=i;
+        }
+        return factorial;
+    }
+
+
+    const num = 5; // Cambia este número para calcular el factorial de otro valor
+    const resultado = calcularFactorial(num);
+    console.log(`El factorial de ${numero} es ${resultado}`);
+}
