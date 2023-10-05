19f0aa32b8c7af5a0b1a1622650321d78de11a06
Ignacio Fernandez <fernandezignacio2000@gmail.com>
2023-10-05 11:11:52 +0200

    015

---
+++ Cliente/015findMaxValue.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let reduceArray = (num) => num.reduce((acc, num) => Math.max(acc,num));{
+    
+}
+let numeros=[1,2,3,4,10,6,7,8,9,5];
+let resultado=reduceArray(numeros);
+console.log(numeros);
