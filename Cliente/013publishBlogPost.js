d611cb5b1b0175fa51e14dfbb139dc013ab13897
Ignacio Fernandez <fernandezignacio2000@gmail.com>
2023-10-05 10:42:17 +0200

    013 comentado y 014 comenzado

--- Cliente/013publishBlogPost.js
+++ Cliente/013publishBlogPost.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /*013publishBlogPost.js Escribe una función flecha que cree una publicación 
 de blog. La función debe tomar un título y un contenido como argumentos 
 obligatorios. También puede tomar una lista de etiquetas opcional.
---
+++ Cliente/014reducearray.js
@@ -0,0 +1,8 @@
+"use strict";
+
+/*014reduceArray.js Crea una función llamada reduceArray que tome un array
+ de números y utilice el método reduce para devolver la suma de todos 
+ los elementos. Utilice las funciones flecha.*/
+
+
+
