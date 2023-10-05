3af9e04e4fc453bc168bb9d8ceccb9a03d4953b7
Ignacio Fernandez <fernandezignacio2000@gmail.com>
2023-10-05 11:00:32 +0200

    013 sin hacer y 14 terminado

--- Cliente/013publishBlogPost.js
+++ Cliente/013publishBlogPost.js
@@ -8,25 +8,3 @@ Si no se proporcionan etiquetas, la publicación se creará sin ellas.*/
 
 
 
-
-const crearBlog = (titulo, contenido, etiquetas) => { //Indicamos los parámetros que entran
-    const publicacion = {
-        titulo: titulo,
-        contenido: contenido,
-        etiquetas: etiquetas
-    };
-return publicacion;
-
-
-};
-//Creamos la primera publicación para el blog
-const miPublicacion = crearBlog('La vida de Lenny', 'Mariwiwi', 'etiquetas');
-
-console.log(miPublicacion);
-
-//Para continuar añadiendo los etiquetas a la publicación
-const otraPublicacion = crearPublicacionBlog('Otra Publicación', 'Contenido de otra publicación', ['etiqueta1', 'etiqueta2']);
-console.log(otraPublicacion);
-
-
-
--- Cliente/014reducearray.js
+++ Cliente/014reducearray.js
@@ -6,3 +6,9 @@
 
 
 
+let reduceArray = (num) => num.reduce((acc, num) => acc + num,0);{
+    
+}
+let numeros=[1,2,3,4,5,6,7,8,9,10];
+let resultado=reduceArray(numeros);
+console.log(numeros);
