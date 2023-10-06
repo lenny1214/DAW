21dfc9176e61976926d32de3a7f30fea4be22231
Ignacio Fernandez <fernandezignacio2000@gmail.com>
2023-10-06 10:13:00 +0200

    ejercicio2empezado

---
+++ Servidor/ejercicio2POO/002empleadosTelefono.php
@@ -0,0 +1,121 @@
+/*002EmpleadoTelefonos.php: Copia la clase del ejercicio anterior y modifícala. Añade
+una propiedad privada que almacene un array de números de teléfonos. Añade los
+siguientes métodos:
+public function anyadirTelefono(int $telefono) : void → Añade un teléfono al array
+public function listarTelefonos(): string → Muestra los teléfonos separados por comas
+public function vaciarTelefonos(): void → Elimina todos los teléfonos*/
+
+<?php
+
+class EmpleadoTeléfono extends Empleado{
+  
+
+    public function __construct(
+      protected  string $nombre, 
+      protected string $apellidos, 
+      protected float $sueldo, 
+      protected  bool $impuestos){
+
+    }
+
+    public function getNombre(){
+        return $this->nombre;
+    }
+
+    public function setNombre($nombre){
+        $this->nombre = $nombre;
+    }
+
+    public function getApellidos(){
+        return $this->apellidos;
+    }
+
+    public function getNombreCompleto():string{
+        return $this->nombre.' '.$this->apellidos;
+    }
+
+    public function setNombreCompleto($nombre, $apellidos) {
+        $this->nombre = $nombre;
+        $this->apellidos = $apellidos;
+    }
+
+
+    public function getSueldo (): float {
+
+        return $this->sueldo;
+    }
+
+    public function setSueldo($sueldo) {
+        $this->sueldo = $sueldo;
+    }
+
+
+   
+    public function debePagarImpuestos(): bool {
+
+        if ($this->sueldo < 3333) {
+            return true;
+            echo "NO pagas impuestos";
+        }else{
+            return false;
+            echo "Pagas impuestos";
+        }
+
+    }
+
+
+    }
+
+    $empleado = new Empleado('Lenny', 'Fernández Vigil-Escalera', 10000, true);
+    echo "nombre completo: ". $empleado->getNombreCompleto(). "<br>";
+    echo "sueldo". $empleado->getSueldo(). "<br>";
+    if ($empleado->debePagarImpuestos() == true) {
+        echo "no debe pagar impuestos: ". $empleado->debePagarImpuestos(). "<br>";
+
+    }else{
+        echo "debe pagar impuestos: ". $empleado->debePagarImpuestos(). "<br>";
+    }
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
