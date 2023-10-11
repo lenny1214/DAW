0ef60d2ddbea494dfc8fefeffe190fa76e810ee3
Ignacio Fernandez <fernandezignacio2000@gmail.com>
2023-10-11 08:49:26 +0200

    012 empezado pero no entiendo como proceder

---
+++ Servidor/ejercicio2POO/ej12/012persona.php
@@ -0,0 +1,46 @@
+<?php
+
+    class Persona{
+       protected string $nombre;
+       protected string $apellidos;
+       protected string $edad;
+       
+
+        public function __construct(
+            protected string $nombre="", 
+            protected string $apellidos="",  
+           ){
+
+            public function getNombre(){
+                return $this->nombre;
+            }
+            
+            public function getApellidos(){
+                return $this->apellidos;
+            }
+
+            public function getNombreCompleto():string{
+                return $this->nombre.' '.$this->apellidos;
+            }
+
+            public function getEdad():string{
+                return $this->edad;
+            }
+
+            public static abstract function toHtml(Persona $p): string;
+        
+
+            public static function __toString(Persona $per): string {
+                $html = "<p>Nombre Completo: ". $per->getNombreCompleto(). "</p>";
+                $html .= "<p>Edad: " . $this->getEdad() . "</p>";
+                return $html;
+            }
+            
+            
+
+
+
+    }
+    }
+
+
