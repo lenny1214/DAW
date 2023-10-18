e5b108b1127fb31d17c01fd7fac7dd470994415e
Ignacio Fernandez <fernandezignacio2000@gmail.com>
2023-10-18 13:46:03 +0200

    comienzo del ejercicio1 y creacion de los demas

---
+++ Servidor/ejercicios3PHP/001server.php
@@ -0,0 +1,53 @@
+/*001server.php: igual que el ejemplo visto en los apuntes, muestra los valores de $_SERVER al ejecutar 
+un script en tu ordenador. Prueba a pasarle parámetros por GET (y a no pasarle ninguno).
+Prepara un formulario (001post.html) que haga un envío por POST y compruébalo de nuevo.
+Crea una página (001enlace.html) que tenga un enlace a 001server.php y comprueba el valor de HTTP_REFERER.*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
