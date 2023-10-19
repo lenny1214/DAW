04297b0ce96078e8399972286d7554bf97e56110
Ignacio Fernandez <fernandezignacio2000@gmail.com>
2023-10-19 12:06:39 +0200

    creacion y comienzo del 5

---
+++ Cliente/Ejercicios1/ejercicios1/ejercicios2/005makeUser.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/*006calculator.js/.html: Crea un objeto calculator con tres métodos:
+read() pide dos valores y los almacena como propiedades de objeto con nombres a y b.
+sum() devuelve la suma de los valores almacenados.
+mul() multiplica los valores almacenados y devuelve el resultado.
+let calculator = {
+  // ... tu código ...
+};
+calculator.read();
+alert( calculator.sum() );
+alert( calculator.mul() );
+*/
