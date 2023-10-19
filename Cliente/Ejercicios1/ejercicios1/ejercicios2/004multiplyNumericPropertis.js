584b3c426b4e8a4ca30ea040412278bdcba8f56a
Ignacio Fernandez <fernandezignacio2000@gmail.com>
2023-10-19 10:00:47 +0200

    comienzo y planteamiento del ejercicio 4 sin terminar

---
+++ Cliente/Ejercicios1/ejercicios1/ejercicios2/004multiplyNumericPropertis.js
@@ -0,0 +1,103 @@
+"use strict";
+
+let menu = {
+    width: 200,
+    height: 300,
+    title: "Mi menú"
+  };
+  
+  multiplyNumeric(menu);
+  
+  
+
+
+
+function multiplyNumeric(obj) {
+
+
+
+
+    
+}
+
+// Después de la llamada
+menu = {
+    width: 400,
+    height: 600,
+    title: "Mi menú"
+  };
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
