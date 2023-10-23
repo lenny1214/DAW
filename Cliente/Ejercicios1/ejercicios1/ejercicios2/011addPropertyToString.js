6e1d161ba5d708c6d988576eca00da8230a03369
Ignacio Fernandez <fernandezignacio2000@gmail.com>
2023-10-23 12:22:10 +0200

    11 y 12 terminado

---
+++ Cliente/Ejercicios1/ejercicios1/ejercicios2/011addPropertyToString.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/*011addPropertyToString.js/.html: ¿Puedo agregar una propiedad a un string?: 
+Considera el siguiente código: let str = "Peter";
+str.test = 5;
+alert(str.test);
+Qué piensas: ¿funcionará? ¿Qué mostrará? ¿Por qué?
+*/
+
+/*Esto no puede realizarse, al principio puede dar lugar a pensar que sí,
+pero investigando y comprobando el código, para poder realizar eso, str deberíamos
+de transformarlo en un objeto.*/
---
+++ Cliente/Ejercicios1/ejercicios1/ejercicios2/012testCuriosity.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/*012testCuriosity.js/.html: Prueba ejecutando esto:
+alert( 9999999999999999 );
+¿Qué está pasando?¿Por qué?
+*/
+
+alert( 9999999999999999 );
+
+/*Lo que ocurre es que redondea. Esto pasa en JS cuando trabajamos con números muy elevados*/
