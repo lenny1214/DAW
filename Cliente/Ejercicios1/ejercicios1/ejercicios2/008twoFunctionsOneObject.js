ce7d85ce327ab9ef6ef9b97b765514657a028331
Ignacio Fernandez <fernandezignacio2000@gmail.com>
2023-10-23 12:13:44 +0200

    comienzo con dudas del 8 y del 9 y terminado el 10

---
+++ Cliente/Ejercicios1/ejercicios1/ejercicios2/008twoFunctionsOneObject.js
@@ -0,0 +1,13 @@
+/*008twoFunctionsOneObject.js/.html: ¿Es posible crear las funciones A y B
+ para que se cumpla new A() == new B()?
+function A() { ... }
+function B() { ... }
+let a = new A();
+let b = new B();
+alert( a == b ); // true
+Si es posible, entonces proporcione un ejemplo de código.
+*/
+
+
+
+"use strict";
---
+++ Cliente/Ejercicios1/ejercicios1/ejercicios2/009otherCalculator.js
@@ -0,0 +1,9 @@
+/*¿Es posible crear las funciones A y B para que se cumpla new A() == new B()?
+function A() { ... }
+function B() { ... }
+let a = new A();
+let b = new B();
+alert( a == b ); // true*/
+
+
+"use strict";
