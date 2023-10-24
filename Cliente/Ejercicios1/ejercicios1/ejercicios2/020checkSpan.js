d156919b7999fb8100fd9f2619d6fb33604302e1
Ignacio Fernandez <fernandezignacio2000@gmail.com>
2023-10-24 13:29:42 +0200

    ejercicio 20 comenzado

---
+++ Cliente/Ejercicios1/ejercicios1/ejercicios2/020checkSpan.js
@@ -0,0 +1,49 @@
+"use strict";
+
+/*
+020checkSpan.js/.html: Escribe una función checkSpam(str) que devuelva
+true si str contiene ‘viagra’ o ‘XXX’, de lo contrario false. 
+La función debe ser insensible a mayúsculas y minúsculas:
+checkSpam('compra ViAgRA ahora') == true
+checkSpam('xxxxx gratis') == true
+checkSpam("coneja inocente") == false
+*/
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
