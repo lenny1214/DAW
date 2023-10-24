a46400e4cfcb10daf302fd480100bde21af489e4
Ignacio Fernandez <fernandezignacio2000@gmail.com>
2023-10-24 12:06:48 +0200

    ejercicio 13 terminado

---
+++ Cliente/Ejercicios1/ejercicios1/ejercicios2/013sumVisitorNumber.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/*013sumVisitorNumbers.js/.html: Crea un script que pida al visitante 
+que ingrese dos números y muestre su suma. P.D. Hay una trampa con los tipos
+de valores.
+*/
+
+
+let num1=+prompt("Dame un número	");
+let num2=+prompt("Dame un número	");
+
+
+
+alert(num1+num2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
