2921f32f9114bf9af9325f54c75c89bac2734506
Ignacio Fernandez <fernandezignacio2000@gmail.com>
2023-10-25 11:48:50 +0200

    comienzo ejercicio 27

---
+++ Cliente/Ejercicios1/ejercicios1/ejercicios2/027transformSnakeToCamel.js
@@ -0,0 +1,58 @@
+"use strict";
+
+/*027transformSnakeToCamel.js/.html: Escribe la función camelize(str) 
+que convierta palabras separadas por _como “mi_cadena_corta” en palabras 
+con mayúscula “miCadenaCorta”. Esto sería: quitar todos los _ y que cada 
+palabra después de _ comience con mayúscula. Ejemplos:
+camelize("background_color") == 'backgroundColor';
+camelize("list_style_image") == 'listStyleImage';
+camelize("_webkit_transition") == 'WebkitTransition';
+P.D. Pista: usa split para dividir el string en un array, transfórmalo y 
+vuelve a unirlo (join).
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
