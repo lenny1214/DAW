e0d4216a513d73a225046bf80b7333ec528c1bb7
Ignacio Fernandez <fernandezignacio2000@gmail.com>
2023-10-30 13:59:07 +0100

    continuación

---
+++ Servidor/ejercicioMezclaInterfaces/consultas.php
@@ -0,0 +1,62 @@
+
+
+
+<?php 
+include_once ("conexion.php");
+
+
+/*$consulta1="";
+$resultado = mysqli_query( $conexion, $consulta1 ) or die ( "Algo ha ido mal en la consulta a la base de datos");
+	echo "<table border='1'";
+    echo "<tr><th>PAIS</th></tr>";
+    if ($resultado){
+        while($fila=mysqli_fetch_assoc($resultado)){
+            echo "<tr><th>" . $fila['NOMBRE'] . $fila['DESPLAZAMIENTO'] .$fila['NRO_CANIONES'] ."</tr></tr>";
+        }
+    }
+
+
+*/
+
+    $consulta2= "SELECT DISTINCT PAIS FROM CLASES WHERE TIPO='ACORAZADO' AND PAIS IN(SELECT PAIS FROM CLASES WHERE TIPO='CRUCERO')";
+
+	$resultado = mysqli_query( $conexion, $consulta2 ) or die ( "Algo ha ido mal en la consulta a la base de datos");
+	echo "<table border='1'";
+    echo "<tr><th>PAIS</th></tr>";
+    if ($resultado){
+        while($fila=mysqli_fetch_assoc($resultado)){
+            echo "<tr><th>" . $fila['PAIS'] . "</tr></tr>";
+        }
+    }
+	// Motrar el resultado de los registro de la base de datos
+	
+	
+
+//ESTO SE SUPONE QUE VA A SER LA CONSULTA 3
+
+
+$consulta3= "SELECT NOMBRE_BATALLA FROM (SELECT B.NOMBRE_BATALLA,CL.PAIS, COUNT(DISTINCT BC.NOMBRE_BARCO)AS num_barcos
+FROM BATALLAS B
+JOIN BARCOS_CLASES BC ON B.NOMBRE=BC.NOMBRE_BARCO
+JOIN CLASES CL ON BC.CLASE
+GROUP BY B.NOMBRE_BATALLA, CL.PAIS)AS BatallasPorPais
+WHERE num_barcos >=3 ";
+
+$resultado2 = mysqli_query( $conexion, $consulta2 ) or die ( "Algo ha ido mal en la consulta a la base de datos");
+	echo "<table border='1'";
+    echo "<tr><th>Nombre Batalla</th></tr>";
+    if ($resultado){
+        while($fila=mysqli_fetch_assoc($resultado)){
+            echo "<tr><th>" . $fila['Nombre Batalla'] . "</tr></tr>";
+        }
+    }
+
+
+
+$consulta4="SELECT PaisesConMaxCaniones.PAIS FROM (SELECT PAIS MAX(NRO_CANIONES) AS max_caniones
+FROM CLASES
+GROUP BY PAIS)AS PaisesConMaxCaniones
+JOIN CLASES ON PaisesConMaxCanCaniones.PAIS AND PaisesConMaxCaniones.max_caniones=CLASES";
+
+$resultado=mysqli_query( $conexion, $consulta4) or die ("No funciona la conexión a la consulta");
+    
--- Servidor/ejercicioMezclaInterfaces/index.php
+++ Servidor/ejercicioMezclaInterfaces/index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -36,56 +37,6 @@
     </header>
 
 
-
-    <?php 
-    // Ejemplo de conexión a base de datos MySQL con PHP.
-	
-	// Datos de la base de datos
-	$usuario = "root";
-	$password = "";
-	$servidor = "localhost";
-	$basededatos = "bbdd_barcos";
-	// creación de la conexión a la base de datos con mysql_connect()
-	$conexion = mysqli_connect( $servidor, $usuario, "" ) or die ("No se ha podido conectar al servidor de Base de datos");
-	
-	// Selección del a base de datos a utilizar
-	$db = mysqli_select_db( $conexion, $basededatos ) or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" );
-
-	// establecer y realizar consulta. guardamos en variable.
-	
-    $consulta= "SELECT DISTINCT PAIS FROM CLASES WHERE TIPO='ACORAZADO' AND PAIS IN(SELECT PAIS FROM CLASES WHERE TIPO='CRUCERO')";
-
-	$resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos");
-	echo "<table border='1'";
-    echo "<tr><th>PAIS</th></tr>";
-    if ($resultado){
-        while($fila=mysqli_fetch_assoc($resultado)){
-            echo "<tr><th>" . $fila['PAIS'] . "</tr></tr>";
-        }
-    }
-	// Motrar el resultado de los registro de la base de datos
-	// Encabezado de la tabla
-	echo "<table border='2'>";
-	echo "<tr>";
-	echo "<th>Tipo de Barco</th>";
-	echo "<th>Desplazamiento</th>";
-    echo "<th>Número de cañones</th>";
-
-	echo "</tr>";
-	
-	// Bucle while que recorre cada registro y muestra cada campo en la tabla.
-	while ($fila = mysqli_fetch_array( $resultado ))
-	{
-		echo "<tr>";
-		echo "<td>" . $columna['TIPO'] . "</td><td>" . $columna['NRO_CANIONES'] . "</td><td>" . $columna['NRO_CANIONES'] . "</td>";
-		echo "</tr>";
-	}
-	
-	echo "</table>"; // Fin de la tabla
-
-	// cerrar conexión de base de datos
-	//mysqli_close( $conexion );?>
-
   <section>
     <div id="inicio">
         <h1>Inicio</h1>
@@ -93,15 +44,21 @@
         <img src = "img/images.png" alt="foto2">
       </section>
 
-
-<?php
-
-
-
+      <?php
+    
+    include_once ("consultas.php");
+    
 
 
 
 ?>
+
+     
+    
+
+
+
+
 
 
 
