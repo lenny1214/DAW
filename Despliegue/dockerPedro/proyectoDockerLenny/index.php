96dd0529ce8068ba0308c991c3ba59b84104958e
Ignacio Fernandez <fernandezignacio2000@gmail.com>
2023-11-27 12:27:48 +0100

    dfsdf

--- Despliegue/dockerPedro/proyectoDockerLenny/index.php
+++ Despliegue/dockerPedro/proyectoDockerLenny/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-  <title>TestDocker</title>
+  <title>Title</title>
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -10,7 +10,7 @@
   <!-- Bootstrap CSS v5.2.1 -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-<!-- Bootstrap JavaScript Libraries -->
+  <!-- Bootstrap JavaScript Libraries -->
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
     integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
   </script>
@@ -18,88 +18,56 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
     integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
   </script>
+  <link rel="stylesheet" href="../css/style.css">
+
+
 </head>
 
 <body>
   <header>
-    <!-- place navbar here -->
-  </header>
+  <nav>
+    <a href="#inicio">Inicio</a>
+    <a href="#ingreso-gasto">Ingreso/Gasto</a>
+    <a href="#ver-saldo">Ver Saldo</a>
+    <a href="#prestamo">Préstamo</a>
+    <a href="#chat">Chat</a>
+</nav> 
+ </header>
   <main>
+  <div style="padding: 16px;">
+    <!-- Tu contenido aquí -->
+    <h2>Bienvenido a tu banca online de confianza</h2>
+    <p>Contenido de la página...</p>
+</div>  
+<?php
+session_start();
 
-  <?php
-
-
-// Datos de conexión a la base de datos
-$host = 'mysql'; // Puede ser 'localhost' si la base de datos está en el mismo servidor
-$dbname = 'TESTDOCKER';
-$username = 'root';
-$password = '';
-$port= 3306;
-
-
-// Crear una conexión a la base de datos
-$conn = new mysqli($host, $username, $password, $dbname,$port);
-
-
-// Verificar la conexión
-if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
+// Verifica si el usuario ha iniciado sesión
+if (!isset($_SESSION['nombre_usuario'])) {
+    header('Location: login.php');
+    exit();
 }
-
-
-// Consulta SQL para crear la tabla 'persona'
-$createTableQuery = "
-    CREATE TABLE IF NOT EXISTS persona (
-        Id int primary key not null,
-        nombre varchar(100),
-        apellido varchar(100)
-    )
-";
-
-
-// Ejecutar la consulta para crear la tabla
-if ($conn->query($createTableQuery) === TRUE) {
-    echo "Tabla creada correctamente. ";
-} else {
-    echo "Error al crear la tabla: " . $conn->error;
-}
-
-
-// Consulta SQL para seleccionar todos los datos de la tabla 'persona'
-$selectDataQuery = "SELECT * FROM persona";
-
-
-// Ejecutar la consulta y mostrar los resultados
-$result = $conn->query($selectDataQuery);
-
-
-if ($result->num_rows > 0) {
-    echo "<br><br>Datos de la tabla 'persona':<br>";
-    while ($row = $result->fetch_assoc()) {
-        echo  " - Nombre: " . $row["nombre"] . " - Apellido: " . $row["apellido"] . "<br>";
-    }
-} else {
-    echo "<br>No hay datos en la tabla 'persona'.";
-}
-
-
-// Cerrar la conexión
-$conn->close();
-
-
-
-
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenido</title>
+</head>
 
+<body>
+    <h2>Bienvenido, <?php echo $_SESSION['nombre_usuario']; ?>!</h2>
+    <p><a href="logout.php">Cerrar Sesión</a></p>
+</body>
 
-
+</html>
   </main>
   <footer>
-    <!-- place footer here -->
   </footer>
-  
+
 </body>
 
 </html>
