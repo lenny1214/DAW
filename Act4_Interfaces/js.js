36e8e1957dadf32df2ad9c7d646dccf64f0a718f
Ignacio Fernandez <fernandezignacio2000@gmail.com>
2024-01-16 12:47:06 +0100

    PORFIIIIIIN

--- Act4_Interfaces/js.js
+++ Act4_Interfaces/js.js
@@ -1,67 +1,93 @@
 "use strict";
-"use strict";
 
-function generarSumaAleatoria() {
-    const num1 = Math.floor(Math.random() * 10) + 1;
-    const num2 = Math.floor(Math.random() * 10) + 1;
+        function generarSumaAleatoria() {
+            const num1 = Math.floor(Math.random() * 10) + 1;
+            const num2 = Math.floor(Math.random() * 10) + 1;
 
-    $("#sumaAleatoriaGenerada").text(`${num1} + ${num2}`);
-    $("#num1").text(num1);
-    $("#num2").text(num2);
-}
-
-function verificarSumaYEnviar() {
-
-    const sumaGenerada = $("#sumaAleatoriaGenerada").text();
-    const sumaIngresada = $("#sumaAleatoria").val();
-
-    if (sumaIngresada.trim() !== sumaGenerada.replace(/\s+/g, '')) {
-        mostrarError("#sumaAleatoria", "La suma ingresada es incorrecta.");
-        return;
-    }
-
-    if (!$("#politicaPrivacidad").prop("checked")) {
-        mostrarError("#politicaPrivacidad", "Debes aceptar la política de privacidad.");
-        return;
-    }
-
-    $("#contactForm").submit();
-    alert("Mensaje enviado con éxito");
-}
-
-
-$(document).ready(function () {
-    $("#imagen1").hover(
-        function () {
-            $(this).attr("src", "../Act4_Interfaces/img/cambioimg1.jpg");
-        },
-        function () {
-            $(this).attr("src", "../Act4_Interfaces/img/img1.jpg");
+            $("#num1").text(num1);
+            $("#num2").text(num2);
         }
-    );
 
-    $("#imagen2").hover(
-        function () {
-            $(this).attr("src", "../Act4_Interfaces/img/cambioimg2.jpg");
-        },
-        function () {
-            $(this).attr("src", "../Act4_Interfaces/img/img2.jpg");
+        function verificarSumaYEnviar() {
+            limpiarErrores();
+
+            if (!camposObligatoriosCompletos()) {
+                return;
+            }
+
+            const sumaIngresada = parseInt($("#sumaAleatoria").val());
+
+            if (!esSumaCorrecta(sumaIngresada)) {
+                mostrarError("#sumaAleatoria", "La suma ingresada es incorrecta.");
+                return;
+            }
+
+            $("#contactForm")[0].submit();
         }
-    );
 
-    $("#imagen3").hover(
-        function () {
-            $(this).attr("src", "../Act4_Interfaces/img/cambioimg3.jpg");
-        },
-        function () {
-            $(this).attr("src", "../Act4_Interfaces/img/img3.jpg");
+        function camposObligatoriosCompletos() {
+            let camposCompletos = true;
+
+            $(".form-control[required]").each(function () {
+                if ($(this).val().trim() === "") {
+                    mostrarError(`#${$(this).attr("id")}`, "Este campo es obligatorio.");
+                    camposCompletos = false;
+                }
+            });
+
+            if (!$("#politicaPrivacidad").prop("checked")) {
+                mostrarError("#politicaPrivacidad", "Debes aceptar la política de privacidad.");
+                camposCompletos = false;
+            }
+
+            return camposCompletos;
         }
-    );
-});
 
-$(document).ready(function () {
-    // ... tu código existente
+        function esSumaCorrecta(sumaIngresada) {
+            const num1 = parseInt($("#num1").text());
+            const num2 = parseInt($("#num2").text());
+            const sumaGenerada = num1 + num2;
 
-    // Llamar a la función para generar la suma aleatoria
-    generarSumaAleatoria();
-});
+            return sumaIngresada === sumaGenerada;
+        }
+
+        function mostrarError(elemento, mensaje) {
+            $(elemento).addClass("is-invalid");
+            $(`${elemento}Error`).text(mensaje);
+        }
+
+        function limpiarErrores() {
+            $(".form-control").removeClass("is-invalid");
+            $("#sumaAleatoriaError, #politicaPrivacidadError").text("");
+        }
+
+        $(document).ready(function () {
+            generarSumaAleatoria();
+
+            $("#imagen1").hover(
+                function () {
+                    $(this).attr("src", "../Act4_Interfaces/img/cambioimg1.jpg");
+                },
+                function () {
+                    $(this).attr("src", "../Act4_Interfaces/img/img1.jpg");
+                }
+            );
+
+            $("#imagen2").hover(
+                function () {
+                    $(this).attr("src", "../Act4_Interfaces/img/cambioimg2.jpg");
+                },
+                function () {
+                    $(this).attr("src", "../Act4_Interfaces/img/img2.jpg");
+                }
+            );
+
+            $("#imagen3").hover(
+                function () {
+                    $(this).attr("src", "../Act4_Interfaces/img/cambioimg3.jpg");
+                },
+                function () {
+                    $(this).attr("src", "../Act4_Interfaces/img/img3.jpg");
+                }
+            );
+        });
